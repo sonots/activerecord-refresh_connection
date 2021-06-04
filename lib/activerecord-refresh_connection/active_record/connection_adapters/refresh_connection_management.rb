b77c43cc5a3a7380d6da02bcdb4195222def0cd4
masato_hi <masato.hirahata@gmail.com>
2021-06-04 13:58:28 +0900

    Fixed an error when legacy_connection_handling is false.

--- lib/activerecord-refresh_connection/active_record/connection_adapters/refresh_connection_management.rb
+++ lib/activerecord-refresh_connection/active_record/connection_adapters/refresh_connection_management.rb
@@ -2,11 +2,14 @@ module ActiveRecord
   module ConnectionAdapters
     class RefreshConnectionManagement
       DEFAULT_OPTIONS = {max_requests: 1}
+      AR_VERSION_6_1 = "6.1".freeze
+      AR_VERSION_6_0 = "6.0".freeze
 
       def initialize(app, options = {})
         @app = app
         @options = DEFAULT_OPTIONS.merge(options)
         @mutex = Mutex.new
+        @ar_version = ActiveRecord.gem_version.to_s
 
         reset_remain_count
       end
@@ -30,22 +33,64 @@ module ActiveRecord
       private
 
       def clear_connections
-        if ActiveRecord::VERSION::MAJOR >= 6
-          if should_clear_all_connections?
-            ActiveRecord::Base.connection_handlers.each_value do |connection_handler|
-              connection_handler.clear_all_connections!
-            end
+        if @ar_version >= AR_VERSION_6_1
+          if legacy_connection_handling?
+            clear_legacy_compatible_connections
           else
-            ActiveRecord::Base.connection_handlers.each_value do |connection_handler|
-              connection_handler.clear_active_connections!
-            end
+            clear_all_roles_connections
+          end
+        elsif @ar_version >= AR_VERSION_6_0
+          clear_legacy_compatible_connections
+        else
+          clear_legacy_connections
+        end
+      end
+
+      def legacy_connection_handling?
+        begin
+          ActiveRecord::Base.legacy_connection_handling
+        rescue NoMethodError
+          false
+        end
+      end
+
+      def all_roles
+        roles = []
+        ActiveRecord::Base.connection_handler.instance_variable_get(:@owner_to_pool_manager).each_value do |pool_manager|
+          roles.concat(pool_manager.role_names)
+        end
+        roles.uniq
+      end
+
+      def clear_all_roles_connections
+        if should_clear_all_connections?
+          all_roles.each do |role|
+            ActiveRecord::Base.clear_all_connections!(role)
           end
         else
-          if should_clear_all_connections?
-            ActiveRecord::Base.clear_all_connections!
-          else
-            ActiveRecord::Base.clear_active_connections!
+          all_roles.each do |role|
+            ActiveRecord::Base.clear_active_connections!(role)
           end
+        end
+      end
+
+      def clear_legacy_compatible_connections
+        if should_clear_all_connections?
+          ActiveRecord::Base.connection_handlers.each_value do |connection_handler|
+            connection_handler.clear_all_connections!
+          end
+        else
+          ActiveRecord::Base.connection_handlers.each_value do |connection_handler|
+            connection_handler.clear_active_connections!
+          end
+        end
+      end
+
+      def clear_legacy_connections
+        if should_clear_all_connections?
+          ActiveRecord::Base.clear_all_connections!
+        else
+          ActiveRecord::Base.clear_active_connections!
         end
       end
 
